ba91bfda44e30e203397ceb09b5ae91ceb9e9486
Parth Goel <parth.goel@thoughtworks.com>
2021-12-20 20:18:56 +0530

    add bootstrap style

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,32 +3,34 @@ import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
     let contents
-    if(props.guessedWords.length===0){
-        contents=(
+    if (props.guessedWords.length === 0) {
+        contents = (
             <span data-test="guess-instructions">
                 Try to guess the secret word
             </span>
         );
-    }else{
-        const guessedWordsRows=props.guessedWords.map((word,index)=>(
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
             <tr key={index} data-test="guessed-word">
                 <td>{word.guessedWord}</td>
                 <td>{word.letterMatchCount}</td>
             </tr>
         ));
-        contents=(
-          <div data-test="guessed-words">
-              <h3>Guessed Words</h3>
-              <table>
-                  <thead>
-                  <tr>Guess</tr>
-                  <tr>Matching Letter</tr>
-                  </thead>
-                  <tbody>
-                  {guessedWordsRows}
-                  </tbody>
-              </table>
-          </div>
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table className="table table-sm">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letter</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {guessedWordsRows}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 
@@ -40,8 +42,8 @@ const GuessedWords = (props) => {
 GuessedWords.propTypes = {
     guessedWords: PropTypes.arrayOf(
         PropTypes.shape({
-            guessedWord:PropTypes.string.isRequired,
-            letterMatchCount:PropTypes.number.isRequired
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
         })
     ).isRequired,
 };
