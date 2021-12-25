10ab5d69c055c7265f28ed26f055f697571d020d
Parth Goel <parth.goel@thoughtworks.com>
2021-12-25 19:42:03 +0530

    add helper fn for letter match b/w guessedWord and secretWord

---
+++ src/helpers/index.js
@@ -0,0 +1,14 @@
+/**
+ * @method getLetterMatchCount
+ * @param guessedWord
+ * @param secretWord
+ * @returns {number}- number of letters matched between guessedWord and secret word
+ */
+
+
+
+export function getLetterMatchCount(guessedWord,secretWord){
+    const secretLetters=secretWord.split('');
+    const guessedLetterSet=new Set(guessedWord);
+    return secretLetters.filter(letter=>guessedLetterSet.has(letter)).length;
+}
